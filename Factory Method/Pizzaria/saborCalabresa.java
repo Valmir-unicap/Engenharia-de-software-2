b991e06d71d89bb46798e9dca9781dc43f932cc4
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-10-09 10:53:44 -0300

    Update saborCalabresa.java

--- Factory Method/Pizzaria/saborCalabresa.java
+++ Factory Method/Pizzaria/saborCalabresa.java
@@ -1,15 +1,15 @@
 public class saborCalabresa implements PizzaStrategy{
 
-    public String sabor = "Pizza de Calabresa";
-    public String ingredientes = "Molho de tomate, Azeitona, Mussarela, Cebola, Cheddar e Orégano";
+    public String _sabor = "Pizza de Calabresa";
+    public String _ingredientes = "Molho de tomate, Azeitona, Mussarela, Cebola, Cheddar e Orégano";
 
     @Override
     public String saborPizza(String sabor){
-        return sabor;
+        return _sabor;
     }
 
     @Override
     public String ingredientesPizza(String ingredientes){
-        return ingredientes;
+        return _ingredientes;
     }
 }
